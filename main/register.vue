4a22d57373e3a1ef1096a7135d07a8251c3ef68c
Agustin Rodeyro <mrodeyro538@alumnos.iua.edu.ar>
2025-08-21 14:04:42 -0300

    inicio de proyecto

---
+++ main/register.vue
@@ -0,0 +1,13 @@
+<template> 
+    <div>
+        <input v-model="name" placeholder="Escribe tu nombre"/>
+        <p>Hola, {{ name }}</p>
+
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const name = ref('');
+</script>
